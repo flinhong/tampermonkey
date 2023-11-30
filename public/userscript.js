d216d62ee6d14dd7a66a4d4bb5fc4f1ac57f4e94
Frank Lin <honglin@duck.com>
2023-11-30 22:55:13 +0800

    update styles

--- public/userscript.js
+++ public/userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         URI identity & styling
 // @namespace    https://frankindev.com/
-// @version      0.0.18
+// @version      0.0.16
 // @description  try to take over the world with styles...
 // @author       Frank Lin
 // @match        *://*/*
