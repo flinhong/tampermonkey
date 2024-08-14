9c8b8445d253122ffc0da775e9ada35922e1bb94
Frank Lin <honglin@dukc.com>
2024-08-14 21:20:31 +0800

    update bilibili

--- public/tampermonkey.js
+++ public/tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         URI identity & styling
 // @namespace    scripts.frankindev.com
-// @version      0.1.3
+// @version      0.1.4
 // @description  try to take over the world with styles...
 // @author       Frank Lin
 // @match        *://*.baidu.com/*
